ece2a1af7c740c11fc01c9fd1b800e6836837b27
Filterrr <295419615@qq.com>
2026-06-01 12:34:48 +0800

    优化后台占用内存

--- lib/manager/background_memory_manager.dart
+++ lib/manager/background_memory_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:fl_clash/clash/clash.dart';
+import 'package:fl_clash/common/common.dart';
 import 'package:fl_clash/common/low_memory_mode.dart';
 import 'package:fl_clash/common/resource_controller.dart';
 import 'package:fl_clash/state.dart';
