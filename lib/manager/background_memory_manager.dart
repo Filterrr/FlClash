ac0d35079b8bcc895b1b9c2e081ea1dd16db5985
Filterrr <295419615@qq.com>
2026-06-04 08:22:21 +0800

    优化内存管理问题

--- lib/manager/background_memory_manager.dart
+++ lib/manager/background_memory_manager.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:fl_clash/clash/clash.dart';
 import 'package:fl_clash/common/common.dart';
-import 'package:fl_clash/common/low_memory_mode.dart';
 import 'package:fl_clash/state.dart';
 
 class BackgroundMemoryManager {
