3d00e04806a9914f2f9e33114bf1a308655a4af1
Filterrr <295419615@qq.com>
2026-06-06 11:29:06 +0800

    修复

--- lib/manager/clash_manager.dart
+++ lib/manager/clash_manager.dart
@@ -117,7 +117,7 @@ class _ClashContainerState extends State<ClashManager> with AppMessageListener {
   @override
   void onLog(Log log) {
     globalState.appController.appFlowingState.addLog(log);
-    if (log.logLevel == LogLevel.error && !isLowMemoryMode) {
+    if (log.logLevel == LogLevel.error && isNormalMemoryMode) {
       globalState.appController.showSnackBar(log.payload ?? '');
     }
     super.onLog(log);
